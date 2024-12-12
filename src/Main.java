bd5efc6e1e90f678cab8bebf42f45845c71130d2
377Nika2004 <borcovaveronika337@gmail.com>
2024-12-12 15:38:02 +0300

    Объекты

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author mia = new Author("Ли", "Мие");
+        Author shimada = new Author("Содзи", "Симада");
+
+        Book worldInDream = new Book ( "Магазин снов мистера Талергута", mia,  2020);
+        Book detective = new Book ( "Хрустальная пирамида", shimada ,  2024);
+
+        System.out.println(worldInDream.getTitle() + " " + worldInDream.getPublicationYear());
+
+        worldInDream.setPublicationYear(2022);
+
+        System.out.println(worldInDream.getTitle() + " " + worldInDream.getPublicationYear());
     }
 }
